b27f45a41225216d848ecc96645fd60bb7fa048c
Sam Rice <samsrice@gmail.com>
2022-08-02 14:15:06 -0700

    Modifies some variables and prints to console

--- variables.js
+++ variables.js
@@ -2,6 +2,8 @@ var favCoffee = "PCC";
 var favAcoustic = "Gibson J-50";
 var favBand = "The Beatles";
 
+console.log(favCoffee)
+
 var myAge = 28;
 var numberOfGuitars = 4;
 var birthYear = 1993;
@@ -9,3 +11,11 @@ var birthYear = 1993;
 var beenToAsia = false;
 var seenGhost = false;
 var hatesFlying = true;
+
+var favCoffee = "Fidalgo";
+var favBand = "Led Zeppelin";
+var favAcoustic = "Harmony H1203";
+
+console.log(favBand);
+
+console.log(myAge);
